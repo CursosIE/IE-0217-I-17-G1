c7e8e8f2870054f8092dce8ee41a6daa8a6ec53a
Alonso Montero <alon182@gmail.com>
2017-03-23 11:10:32 -0600

    Created header file for required functions

---
+++ lab0/include/functions.h
@@ -0,0 +1,7 @@
+/****************************************************************************
+ *  Author: Alonso Montero Fuentes <alon182@gmail.com>                 	    *
+ ****************************************************************************/
+void clearScreen();
+void moveScreen();
+void printScreen();
+void wait(unsigned int time);

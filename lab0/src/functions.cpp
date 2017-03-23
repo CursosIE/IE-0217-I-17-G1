8f89a0c55b7521aa7690ba6a8806fefc64ce7f89
Alonso Montero <alon182@gmail.com>
2017-03-23 11:11:11 -0600

    Created required functions except for printScreen

---
+++ lab0/src/functions.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+void clearScreen(int height)
+{
+    // Enters multiple lines to clear the screen
+    for(int i=1; i <= height; i=i + 1){
+	cout << endl;
+    }
+}
+
+void moveScreen()
+{
+    cout << endl;
+}
+
+//void printScreen(){}
+
+void wait(unsigned int time)
+{
+    sleep(time);
+}
